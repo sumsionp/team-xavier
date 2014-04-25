373f59c68a1f0f5f402dc5fd72da59e8340d47e2
Peter Sumsion <sumsionp@gmail.com>
2014-04-24 20:38:16 -0600

    Update team tests to remove team_lead

--- spec/views/teams/new.html.erb_spec.rb
+++ spec/views/teams/new.html.erb_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe "teams/new" do
   before(:each) do
     assign(:team, stub_model(Team,
-      :name => "MyString",
-      :team_lead => ""
-    ).as_new_record)
+      :name => "MyString"
+   ).as_new_record)
   end
 
   it "renders new team form" do
@@ -14,7 +13,6 @@ describe "teams/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", teams_path, "post" do
       assert_select "input#team_name[name=?]", "team[name]"
-      assert_select "input#team_team_lead[name=?]", "team[team_lead]"
     end
   end
 end
